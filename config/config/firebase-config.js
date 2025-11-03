882f8d2c40ebe2fff2a5eaba47276e453fc2c3ed
umittasdemir1 <63965329+umittasdemir1@users.noreply.github.com>
2025-11-03 20:53:38 +0300

    Create firebase-config.js

---
+++ config/config/firebase-config.js
@@ -0,0 +1,8 @@
+window.__FIREBASE_CONFIG = {
+  authDomain: "retail-quiz-4bb8c.firebaseapp.com",
+  projectId: "retail-quiz-4bb8c",
+  storageBucket: "retail-quiz-4bb8c.appspot.com",
+  messagingSenderId: "656506684656",
+  appId: "1:656506684656:web:a3e97785fdbe50737f6e35",
+  measurementId: "G-WMBNP0BZ27"
+};
