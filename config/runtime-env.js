466f6fa4484e006df78b2bdfb61a15ecb0e52219
umittasdemir1 <63965329+umittasdemir1@users.noreply.github.com>
2025-11-11 17:44:52 +0300

    Delete config directory

--- config/runtime-env.js
+++
@@ -1,7 +0,0 @@
-// Runtime Environment Configuration
-// This file is auto-generated or manually created for local development
-
-window.__RUNTIME_CONFIG = {
-  VITE_FIREBASE_API_KEY: '${VITE_FIREBASE_API_KEY}',
-  GA_MEASUREMENT_ID: 'G-WMBNP0BZ27'
-};
